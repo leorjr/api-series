f2511c35a78e1e51143f844e5195d7879a544ade
Leonardo Rodrigues <leonardorodrigues@Air-de-Leonardo.local>
2022-05-05 15:25:21 -0300

    feat: created route post

--- app/controllers/series_controller.py
+++ app/controllers/series_controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from app.models.series_model import Series
 
 
@@ -8,5 +8,12 @@ def init_app(app: Flask):
     def list_all_series():
         data = Series.list_all_series()
         return jsonify(data), 200
+
+    @app.post("/series")
+    def create_serie():
+        data = request.json
+        serie = Series(data)
+        new_serie = Series.create_serie(serie)
+        return new_serie, 200
 
     return app
--- app/models/series_model.py
+++ app/models/series_model.py
@@ -1,6 +1,8 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
+from psycopg2 import sql
+from typing import Union
 
 load_dotenv()
 
@@ -38,8 +40,13 @@ def create_table():
 
 class Series():
 
-    def __init__(self, data) -> None:
-        self.id, self.serie, self.seasons, self.released_date, self.genre, self.imdb_rating = data
+    def __init__(self, data: Union[tuple, dict]) -> None:
+        if type(data) is tuple:
+            self.id, self.serie, self.seasons, self.released_date, self.genre, self.imdb_rating = data
+
+        elif type(data) is dict:
+            for key, value in data.items():
+                setattr(self, key, value)
 
     @staticmethod
     def list_all_series():
@@ -62,3 +69,31 @@ class Series():
         list_results = [Series(serie).__dict__ for serie in results]
 
         return list_results
+
+    def create_serie(self):
+
+        columns = [sql.Identifier(key) for key in self.__dict__.keys()]
+        values = [sql.Literal(value) for value in self.__dict__.values()]
+
+        conn = psycopg2.connect(**configs)
+        cur = conn.cursor()
+
+        query = sql.SQL("""
+            INSERT INTO
+                ka_series (id, {columns})
+            VALUES
+                (DEFAULT, {values})
+            RETURNING *
+        """).format(columns=sql.SQL(',').join(columns), values=sql.SQL(',').join(values))
+
+        cur.execute(query)
+
+        fetch_result = cur.fetchone()
+
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        serialized_data = Series(fetch_result).__dict__
+
+        return serialized_data
